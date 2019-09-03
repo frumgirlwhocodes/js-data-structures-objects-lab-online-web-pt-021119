0929d31f3785cba85b1292ae32f3cc8433086820
lieby weintraub <liebahinda@gmail.com>
2019-09-03 16:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function  destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   
 }; 
 function deleteFromDriverByKey(driver, key) {
- const newDriver= Object.assign({})
+ const newDriver= Object.assign({}, driver)
 };
