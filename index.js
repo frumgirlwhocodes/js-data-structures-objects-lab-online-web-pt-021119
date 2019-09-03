e9b8576e81f8e38c3c3d5cf4f84414d6bd1ee744
lieby weintraub <liebahinda@gmail.com>
2019-09-03 15:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver = {}
 
 function  updateDriverWithKeyAndValue(driver, key, value) {
-   const newDriver= Object.assign()
+   const newDriver= Object.assign({}, )
   
   return newDriver 
 };
